f48923b42c36981853db15281f5b976de41a1802
lukmaan-b <lukmaan.butt@gmail.com>
2021-01-12 15:34:14 +0000

    Fix: join with id

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const io = require('socket.io')(PORT);
 
 io.on('connection', (socket) => {
   const id = socket.handshake.query.id;
+  socket.join(id);
   socket.on('send-message', (data) => {
     for (const r of data.recipents) {
       socket.broadcast.to(r).emit('message-recieve', data);
