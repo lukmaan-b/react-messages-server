ca7bf86e20284725ebad722eabbc4160b91d252f
lukmaan-b <lukmaan.butt@gmail.com>
2021-01-12 15:32:57 +0000

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+const PORT = process.env.PORT || 5000;
+const io = require('socket.io')(PORT);
+
+io.on('connection', (socket) => {
+  const id = socket.handshake.query.id;
+  socket.on('send-message', (data) => {
+    for (const r of data.recipents) {
+      socket.broadcast.to(r).emit('message-recieve', data);
+    }
+  });
+});
